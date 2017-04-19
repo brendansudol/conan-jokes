01a26620a9a03c13e729349bf9a255b525b95a3c
brendan sudol <brendansudol@gmail.com>
2017-04-18 22:01:39 -0400

    use algoliasearch module

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import React, { Component } from 'react'
-import {
-  Configure,
-  Hits,
-  InstantSearch,
-  Pagination,
-  RefinementList,
-  SearchBox,
-} from 'react-instantsearch/dom'
+import Algolia from 'algoliasearch'
 
-const Item = ({ hit }) => <div className='my1'>{hit.body}</div>
+const client = Algolia('6L4BP18C7G', '59e5d053f16aa77c6b7cd94ccb83746a')
+const index = client.initIndex('jokes')
 
 class App extends Component {
+  state = {
+    results: null,
+    search: '',
+  }
+
+  componentDidMount() {
+    this.getResults('Trump')
+  }
+
+  getResults = query => {
+    index.search(query, (err, results) => {
+      this.setState({ results })
+    })
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    const { search } = this.state
+    this.getResults(search)
+  }
+
+  updateSearch = e => {
+    this.setState({ search: e.target.value })
+  }
+
   render() {
+    const { results, search } = this.state
+
     return (
-      <div className='p3'>
-        <InstantSearch
-          appId="6L4BP18C7G"
-          apiKey="59e5d053f16aa77c6b7cd94ccb83746a"
-          indexName="jokes"
-        >
-          <Configure hitsPerPage={25} />
-          <SearchBox defaultRefinement="Trump" />
-          <RefinementList attributeName="rating" />
-          <Hits hitComponent={Item} />
-          <Pagination />
-        </InstantSearch>
+      <div className='p3 mx-auto' style={{ maxWidth: 600 }}>
+        <form className='mb3 flex' onSubmit={this.submitHandler}>
+          <label className='hide'>Search</label>
+          <input
+            type='text'
+            className='flex-auto field rounded-left'
+            value={search}
+            onChange={this.updateSearch}
+          />
+          <button
+            type='submit'
+            className='btn rounded-right border black bg-silver'
+          >
+            Go
+          </button>
+        </form>
+        {results && results.hits.map(hit => (
+          <div key={hit.id} className='mb3'>{hit.body}</div>
+        ))}
       </div>
     )
   }
